e36d0b529950157ebe515f93761cd1fa06a917f5
Mimi Netto <michnetto249@gmail.com>
2020-11-01 16:03:01 -0500

    heroku

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -1,4 +1,4 @@
-import React, {Component, setState} from 'react'
+import React, { Component } from 'react'
 // import { Button, Modal, ModalBody, ModalFooter } from 'react-bootstrap'
 // import ModalHeader from 'react-bootstrap/ModalHeader'
 import Button from 'react-bootstrap/Button'
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button'
 const display = { display: 'block'};
 const hide = { display: 'none'};
 
-class EditModal extends React.Component {
+class EditModal extends Component {
   constructor(props) {
     super(props);
     this.toggle = this.toggle.bind(this);
@@ -25,7 +25,7 @@ class EditModal extends React.Component {
       <div className="modal" style={this.state.toggle ? display : hide}>
       <div className="modal-content">
       <div className="modal-header">
-        <a className="btn-flat right" onClick={this.toggle}><h1>X</h1></a>
+        <a className="right" onClick={this.toggle}><h1>X</h1></a>
         </div>
         </div>
       </div>
